fafc904abc78ac31a2c3b88c69d9434f2eb7e3c8
Joshua Concepcion <joshuarconcepcion@gmail.com>
2025-09-09 13:24:16 -0700

    Edited colors for sidebar and dashboard elements

--- src/ui/dashboard.js
+++ src/ui/dashboard.js
@@ -2,13 +2,34 @@ export function renderDashboard(container) {
     container.appendChild(renderDashboardContainer());
 }
 
-const renderDashboardBalance = () => {
-    
-}
-
 const renderDashboardContainer = () => {
     const dashboardContainer = document.createElement('div');
     dashboardContainer.classList.add('dashboard-container');
 
+    const dashboardContainerLeft = document.createElement('div');
+    dashboardContainerLeft.classList.add('dashboard-container-left');
+
+    const dashboardContainerRight = document.createElement('div');
+    dashboardContainerRight.classList.add('dashboard-container-right');
+
+    dashboardContainer.append(dashboardContainerLeft, dashboardContainerRight);
+    dashboardContainerLeft.append(renderDashboardBalance());
+
     return dashboardContainer;
+}
+
+const renderDashboardBalance = () => {
+    const dashboardBalanceContainer = document.createElement('div');
+    dashboardBalanceContainer.classList.add('dashboard-balance-container');
+
+    const balanceHeader = document.createElement('p');
+    balanceHeader.classList.add('balance-header');
+    balanceHeader.innerHTML = 'Total Balance'
+
+    const balanceNumber = document.createElement('p');
+    balanceNumber.classList.add('balance-number');
+    balanceNumber.innerHTML = '$42,124<span id="balance-decimals">.00</span>';
+
+    dashboardBalanceContainer.append(balanceHeader, balanceNumber);
+    return dashboardBalanceContainer;
 }
